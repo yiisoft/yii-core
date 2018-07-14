680b1d4400b43510a39dc6923fb80e2d953ccf7c
Andrii Vasyliev <sol@hiqdev.com>
2018-07-14 10:08:28 +0000

    renamed framework dir to `src`

--- framework/views/_addComments.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Creates a call for the method `yii\db\Migration::createTable()`
- */
-/* @var $table string the name table */
-/* @var $tableComment string the comment table */
-?>        $this->addCommentOnTable('<?= $table ?>', '<?= $tableComment ?>');
