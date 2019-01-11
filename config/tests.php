dedf7d9aca109ea777747287de920e423f8d1a46
Andrii Vasyliev <sol@hiqdev.com>
2019-01-11 17:51:34 +0000

    Fixed aliases in tests config

--- config/tests.php
+++ config/tests.php
@@ -5,9 +5,8 @@ $_ENV['TEST_RUNTIME_PATH'] = $_ENV['TEST_RUNTIME_PATH'] ?? dirname(__DIR__) . '/
 return [
     'app' => [
         'id' => 'testapp',
-        'aliases' => [
-            '@runtime'           => $_ENV['TEST_RUNTIME_PATH'],
-            '@yii/tests/runtime' => $_ENV['TEST_RUNTIME_PATH'],
-        ],
+    ],
+    'aliases' => [
+        '@runtime'           => $_ENV['TEST_RUNTIME_PATH'],
     ],
 ];
